e81f2ab9dce07897e83cdf995640e128dcf9c5dd
Adithya Sapalya <adithyasapalya30@gmail.com>
2025-12-25 19:35:46 +0530

    genrated problem

--- tmc.py
+++ tmc.py
@@ -1,5 +1,16 @@
 import random
 
-OPERATORS = ['+', '-', '*', '/']
+OPERATORS = ['+', '-', '*']
 MIN_OPS =  3
 MAX_OPS =  12
+
+def generate_problem():
+    left = random.randint(MIN_OPS, MAX_OPS)
+    right = random.randint(MIN_OPS, MAX_OPS)
+    operator = random.choice(OPERATORS)
+
+    expr = str(left) + ' ' + operator + ' ' + str(right)
+    print(expr)
+    return expr
+
+generate_problem()
